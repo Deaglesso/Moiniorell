1e91171369339472221478e2b920f762743b9e15
Deaglesso <seid.beam@gmail.com>
2024-02-23 23:45:38 +0400

    Final Push

--- Moiniorell/src/Core/Moiniorell.Domain/obj/Debug/net6.0/Moiniorell.Domain.AssemblyInfo.cs
+++ Moiniorell/src/Core/Moiniorell.Domain/obj/Debug/net6.0/Moiniorell.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Moiniorell.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b35846e7c1e125d589d462e4180ee227f0df37d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Moiniorell.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Moiniorell.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
